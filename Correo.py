91122e8acd773f6e5e61f3e794493d40f6dfa9ce
ovillalbaUsa <91650236+ovillalbaUsa@users.noreply.github.com>
2022-01-20 16:09:02 -0500

    Formato de correo con formato HTML

--- Correo.py
+++ Correo.py
@@ -5,6 +5,7 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from datetime import datetime
 
+
 def execution_time(func):
     def wrapper(*args, **kwargs):
         initial_time = datetime.now()
@@ -12,7 +13,9 @@ def execution_time(func):
         final_time = datetime.now()
         time_elapsed = final_time - initial_time
         print(f'Pasaron {time_elapsed.total_seconds()} segundos')
+
     return wrapper
+
 
 print("Esto es la rama v_2.0")
 
@@ -21,9 +24,8 @@ from termcolor import colored"""
 
 """Cosas por mejorar:
 1) guardar la contraseña en un lugar seguro
-2) crear emailñs con formatos
+2) Poner el logo en el correo electrónico
 3) correspondencia con diferentes archivos
-4) enviar imagenes
 5) evaluar los excel para automatizar los rangos
 6) arreglar lo de los inmuebles con más correos"""
 
@@ -31,11 +33,10 @@ from termcolor import colored"""
 
 
 def obtener_valores() -> list:
-
     wb = load_workbook('Formato correos nuevo.xlsx')
     ws = wb.active
     lista_completa = []
-    filas_total = 2 # número de la última fila
+    filas_total = 3  # número de la última fila
     columnas_total = 9
     for row in range(2, filas_total + 1):  # Se tienen que cambiar
         laux = []
@@ -64,6 +65,7 @@ def limpiar_datos(lista: list) -> tuple:
         lclean.append(laux)
     return tuple(lclean)
 
+
 @execution_time
 def enviar_mensaje(lista: tuple) -> None:
     count = 0
@@ -81,48 +83,52 @@ def enviar_mensaje(lista: tuple) -> None:
         fecha = '31 de diciembre de 2021'
 
         mensaje = MIMEMultipart()
-        mensaje['Subject'] = f'{asunto} - INMUEBLE {inmueble}'
-        '''cuerpo = (f"""RECORDATORIO COBRO {tipo_cobro.upper()}\n\n\n
-Apreciado(a) Señor (a),\n\n\n
-La obligación que usted tiene como propietario/tenedor del Inmueble de la referencia, se encuentra actualmente en cobro {tipo_AP.upper()} bajo la supervisión de CARVEL SOLUCIONES JURÍDICAS Y ADMINISTRATIVAS S.A.S., lo anterior debido al incumplimiento en sus pagos de las cuotas de administración que a {fecha} se discriminan a continuación:\n\n\n
-Saldo a Capital: ${capital_inicial + costas:,.0f}\n
-Honorarios Cobro {tipo_cobro}: ${honorarios:,.0f}\n
-Total Deuda: ${total:,.0f}\n\n\n
-Solicitamos cancelar el valor adeudado y si ya realizó el pago, le pedimos que por favor se comunique con nosotros para verificar con los respectivos soportes en la administración.\n
-Es necesario que ponga al día la obligación, debido a que si usted hace abonos parciales estos deben estar autorizados previamente, ya que es usted acreedor al pago de honorarios sobre todos los abonos que efectúe.\n
-AÚN PUEDE EVITAR ESTA SITUACIÓN, COMUNÍQUESE DE INMEDIATO y así impedir más cargos, con el fin de efectuar el pago y/o llegar a un acuerdo. Estaremos atentos a cualquier inquietud en nuestras líneas 3107619044 o en el correo electrónico carvel.soluciones@gmail.com.\n\n\n
-Cordialmente,\n\n\n
-CAROLINA VELÁSQUEZ\n
-Departamento Jurídico""")'''
+        mensaje['Subject'] = f'COBRO {tipo_cobro.upper()} - {asunto} - INMUEBLE {inmueble}'
 
         cuerpo = f"""\
+        <!DOCTYPE html>
         <html>
           <head></head>
           <body>
               <header>
-                hola mundo
+                  <a href="">
+                      <img src="C:Users\AMD\Documents\CARVEL\Python\Correos\logo.png" alt="logo" width="100" height="100">
+                  <h3><b><ins>RECORDATORIO COBRO {tipo_cobro.upper()}</h3><br><br>
               </header> 
               <main>
                 <section>
-                    <article>
-                    
+                  <p>Apreciado(a) Señor (a),<br><br><br></p>
+                  <p>La obligación que usted tiene como propietario/tenedor del Inmueble de la referencia, se encuentra actualmente en cobro {tipo_AP.upper()} bajo la supervisión de CARVEL SOLUCIONES JURÍDICAS Y ADMINISTRATIVAS S.A.S., lo anterior debido al incumplimiento en sus pagos de las cuotas de administración que a <b>{fecha}</b> se discriminan a continuación:</p><br><br>
+                  <p>Saldo a Capital: <b>${capital_inicial + costas:,.0f}</p>
+                  <p>Honorarios Cobro {tipo_cobro}: <b>${honorarios:,.0f}</p>
+                  <p>Total Deuda: <b>${total:,.0f}</p><br><br>
+                  <p>Solicitamos cancelar el valor adeudado y si ya realizó el pago, le pedimos que por favor se comunique con nosotros para verificar con los respectivos soportes en la administración.</p>
+                  <p>Es necesario que ponga al día la obligación, debido a que si usted hace abonos parciales estos deben estar autorizados previamente, ya que es usted acreedor al pago de honorarios sobre todos los abonos que efectúe.</p>
+                  <p><b>AÚN PUEDE EVITAR ESTA SITUACIÓN, COMUNÍQUESE DE INMEDIATO</b> y así impedir más cargos, con el fin de efectuar el pago y/o llegar a un acuerdo.
+                  Estaremos atentos a cualquier inquietud en nuestras líneas <b>3107619044</b> o en el correo electrónico
+                  <a href = "mailto: carvel.soluciones@gmail.com">carvel.soluciones@gmail.com.</a><br><br><br>
+                  <p>
+                  Cordialmente,<br><br><br>
+                  <b>Carolina Velásquez</b><br>
+                  <b>Departamento Jurídico</b> 
+                  </p>
                 </section>
               </main> 
           </body>
         </html>
         """
-        mensaje.attach(MIMEText(cuerpo, 'html'))
+        mensaje.attach(MIMEText(cuerpo, 'html', 'utf-8'))
         texto = mensaje.as_string()
         servidor = smtplib.SMTP('smtp.gmail.com', 587)
         servidor.starttls()
-        servidor.login('ovillalbaunal@gmail.com', 'la cambio despues del parcial')
-        servidor.sendmail('ovillalbaunal@gmail.com', correos, texto)
+        servidor.login('carvel.soluciones@gmail.com', 'Chico2021*')
+        servidor.sendmail('carvel.soluciones@gmail.com', correos, texto)
         servidor.quit()
 
         count += 1
         print(f'{count}. Envio exitoso')
 
-    print(f'\nFin del proceso\nTotal correos enviados: {count}')
+    print(f'\nFin del proceso\nTotal correos enviados: {count}\n')
 
 
 # ------------------------------------------------------
@@ -135,6 +141,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-
